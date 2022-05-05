5cbc407c1aa1fdf2b715ce083b44d4d9545522cf
EliasTechnik <elias-wagner.online@gmx.de>
2022-05-05 15:31:06 +0200

    fixed inheritance

--- esp32-simple-ui/include/simpleUI.h
+++ esp32-simple-ui/include/simpleUI.h
@@ -5,6 +5,8 @@
 
 using namespace std; //to use string
 
+enum UIEaligne{UIEAcenter,UIEAleft,UIEAright,UIEAtop,UIEAbottom};
+
 class dimensions{
     protected:
         unsigned int pos_x;
@@ -12,45 +14,54 @@ class dimensions{
         unsigned int width;
         unsigned int height;
     public:
-        dimensions(unsigned int _pos_x, unsigned int _pos_y, unsigned int _width, unsigned int _height);
+        dimensions(unsigned int _pos_x = 0, unsigned int _pos_y = 0, unsigned int _width = 0, unsigned int _height = 0);
         unsigned int getX();
         unsigned int getY();
         unsigned int getWidth();
         unsigned int getHeight();
+        void setDimension(unsigned int _pos_x = 0, unsigned int _pos_y = 0, unsigned int _width = 0, unsigned int _height = 0);
 };
 
-class uielement : dimensions{
+class uielement : public dimensions{
     protected:
         bool visible;
         U8G2 display;
         string name;
+        bool focus; //true if element has focus (flash inverted)
     public:
-        uielement(unsigned int _pos_x, unsigned int _pos_y, unsigned int _width, unsigned int _height, bool isVisible = true);
+        uielement(unsigned int _pos_x = 0, unsigned int _pos_y = 0, unsigned int _width = 0, unsigned int _height = 0, bool isVisible = true);
         void setVisible(bool isVisible);
         bool getVisible();
         void setDisplay(U8G2 _display);
         void setName(string _name);
-        void draw(); //should be overwritten by every child class which inherrits from uielement
+        void setFocus(bool _focus = true);
+        virtual void draw(); //should be overwritten by every child class which inherrits from uielement
 };
 
-class icon : uielement{
-    private:
+class icon : public uielement{
+    protected:
         unsigned char* bitmap; 
     public:
-        icon(unsigned char* _bitmap, unsigned int _pos_x, unsigned int _pos_y, unsigned int _width, unsigned int _height, bool isVisible = true);
-        unsigned char* getBitmap();      
+        icon(unsigned char* _bitmap, unsigned int _pos_x = 0, unsigned int _pos_y = 0, unsigned int _width = 0, unsigned int _height = 0, bool isVisible = true);
+        unsigned char* getBitmap();  
+        void draw();    
 };
 
-class label : uielement{
-    private:
+class label : public uielement{
+    protected:
         string text;
+        UIEaligne aligne;
     public:
         label(string _text, unsigned int _pos_x, unsigned int _pos_y, unsigned int _width, unsigned int _height, bool isVisible = true);
-
+        void setText(string _text);
+        void setAligne(UIEaligne _aligne);
+        UIEaligne getAligne();
+        string getText();
+        //TODO: setFont, setSize, setColor, setUnderline, setItallic, setBold
 };
 
 class uipage{
-    private:
+    protected:
         uielement* elist; //dynamic array  
         string name;
         unsigned int ecount; 
@@ -64,7 +75,7 @@ class uipage{
 };
 
 class ui{
-    private:
+    protected:
         unsigned int x;
         unsigned int y;
         unsigned int pagecount; //counts the number of available ui pages
@@ -78,4 +89,5 @@ class ui{
         uipage getPage(string name);
         void displayPage(string name);
         void displayPage(unsigned int index);
+        void updateUI(); //updates the display, should be called several times a second (30x for 30 FPS)
 };
--- esp32-simple-ui/src/simpleUI.cpp
+++ esp32-simple-ui/src/simpleUI.cpp
@@ -3,10 +3,7 @@
 
 //dimmensions
 dimensions::dimensions(unsigned int _pos_x, unsigned int _pos_y, unsigned int _width, unsigned int _height){
-    pos_x=_pos_x;
-    pos_y=_pos_y;
-    width=_width;
-    height=_height;
+    this->setDimension(_pos_x,_pos_y,_width,_height);
 };
 unsigned int dimensions::getX(){
     return pos_x;
@@ -20,23 +17,43 @@ unsigned int dimensions::getWidth(){
 unsigned int dimensions::getHeight(){
     return height;
 };
+void dimensions::setDimension(unsigned int _pos_x, unsigned int _pos_y, unsigned int _width, unsigned int _height){
+    pos_x=_pos_x;
+    pos_y=_pos_y;
+    width=_width;
+    height=_height;
+};
 
 //uielement
-uielement::uielement(unsigned int _pos_x, unsigned int _pos_y, unsigned int _width, unsigned int _height, bool isVisible = true){
-    using dimensions::dimensions(_pos_x, _pos_y, _width, _height);
+uielement::uielement(unsigned int _pos_x, unsigned int _pos_y, unsigned int _width, unsigned int _height, bool isVisible){
+    this->setDimension(_pos_x,_pos_y,_width,_height);
+    visible = isVisible;
+    focus = false;
 };
 void uielement::setVisible(bool isVisible){
-
+    visible = isVisible;
 };
 bool uielement::getVisible(){
-
+    return visible;
 };
 void uielement::setDisplay(U8G2 _display){
-
+    display = _display;
 };
 void uielement::setName(string _name){
-
+    name = _name;
 };
-void uielement::draw(){
+void uielement::setFocus(bool _focus){
+    focus = _focus;
+};
 
-}; //should be overwritten by every child class which inherrits from uielement
+//icon
+icon::icon(unsigned char* _bitmap, unsigned int _pos_x, unsigned int _pos_y, unsigned int _width, unsigned int _height, bool isVisible){
+    this->setDimension(_pos_x,_pos_y,_width,_height);
+    this->setVisible(isVisible);
+};
+unsigned char* icon::getBitmap(){
+    return bitmap;
+}
+void icon::draw(){
+    display.drawXBM(pos_x,pos_y,width,height,bitmap);
+}
